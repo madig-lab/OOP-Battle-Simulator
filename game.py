d51c4175ebb1811340300bdecc7459d43d5d779c
madig-lab <madison.m.graham24@gmail.com>
2025-09-03 13:25:12 -0500

    added a hero

--- game.py
+++ game.py
@@ -7,18 +7,21 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
 
     # Create a hero
-    hero = Hero("Aragorn")
+    hero = Hero("Nightwing")
+   
 
     # Create goblins ༼ ºل͟º ༽ ༼ ºل͟º ༽ ༼ ºل͟º ༽
     goblins = [Goblin(f"Goblin {i+1}") for i in range(3)]
 
     # Keep track of how many goblins were defeated
     defeated_goblins = 0
+    total_damage=0
+    rounds= 0
 
     # Battle Loop 
     while hero.is_alive() and any(goblin.is_alive() for goblin in goblins):
         print("\nNew Round!")
-        
+        rounds +=1
         # Hero's turn to attack
         target_goblin = random.choice([goblin for goblin in goblins if goblin.is_alive()])
         damage = hero.strike()
@@ -42,7 +45,8 @@ def main():
         print(f"\nThe hero has defeated all the goblins! ༼ ᕤ◕◡◕ ༽ᕤ")
     else:
         print(f"\nThe hero has been defeated. Game Over. (｡•́︿•̀｡)")
-
+    print(f"\nTotal damage delt: {total_damage}")
+    print(f"total rounds fought:{rounds}")
     # Final tally of goblins defeated
     print(f"\nTotal goblins defeated: {defeated_goblins} / {len(goblins)}")
 
--- hero.py
+++ hero.py
@@ -5,6 +5,7 @@ class Hero:
     O=('-'Q)
 
     Attributes:
+
         name: The name of our adventurer.
         hp: The current health value.
         strength: The amount of damage the hero can deal.
@@ -14,15 +15,24 @@ class Hero:
     
     def __init__(self, name):
         #TODO Set the hero's name.
+        self.name = name  
+        self.health = 264
+        self.attack_power = 67
         #TODO Set the hero's health. You might give the hero more health than a goblin.
         #TODO Set the hero's attack power. Should it be more consistent than the goblin's?
     
 
     def strike(self):
+       return random.randint(1, self.attack_power)
         # TODO Implement the hero's attack logic. It could be stronger or more consistent than a goblin's.
-    
+        
     def receive_damage(self, damage):
         # TODO Implement take_damage
+        self.health -= damage
+        print(f"{self.name} takes {damage} damage. Health is now {self.health}.")
+
         # TODO We should prevent health from going into the NEGATIVE
     
     #TODO define is_alive
+    def is_alive(self):
+        return self.health > 0
